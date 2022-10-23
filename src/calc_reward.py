4888011c9098e4ce686c1e725c0cbfccb6ef1324
rkaganda <rkaganda@gmail.com>
2022-10-22 22:54:56 -0700

    init

---
+++ src/calc_reward.py
@@ -0,0 +1,292 @@
+import pandas as pd
+import json
+from IPython.display import JSON
+from os import listdir
+from os.path import isfile, join
+
+import plotly.express as px
+import plotly.graph_objs as go
+from plotly.graph_objs.scatter.marker import Line
+
+pd.options.display.max_rows = 4000
+pd.options.display.max_seq_items = 4000
+pd.options.display.max_columns = 4000
+
+
+def load_file(file_path):
+    with open(file_path) as f:
+        file_dict = json.load(f)
+
+    JSON(file_dict)
+
+    return file_dict
+
+
+def create_eval_df(file_dict):
+    # reform model_output
+    d_ = dict()
+
+    for frame_idx, (frame_key, frame_row) in enumerate(file_dict['model_output'].items()):
+        row = dict()
+        for row_idx, (key, item) in enumerate(frame_row.items()):
+            if isinstance(item, list):
+                for idx, value in enumerate(item):
+                    row["{}_{}".format(key, idx)] = value
+            else:
+                row["{}".format(key)] = item
+        d_[frame_idx] = row
+
+    # JSON(d_)
+
+    eval_df = pd.DataFrame.from_dict(d_, orient='index')
+
+    # display(eval_df.head())
+    # display(eval_df.tail())
+
+    return eval_df
+
+
+def create_norm_state_df(file_dict):
+    norm_dict = {}
+
+    for index, item in enumerate(file_dict['normalized_states']):
+        row = dict()
+        for idx, value in enumerate(item['input'] + item['game']):
+            row[idx] = value
+        norm_dict[index] = row
+
+    # JSON(norm_dict)
+
+    norm_state_df = pd.DataFrame.from_dict(norm_dict, orient='index')
+
+    # display(norm_state_df.head())
+    # display(norm_state_df.tail())
+
+    return norm_state_df
+
+
+def calcuate_actual_state_df(file_dict):
+    actual_state_dict = {}
+
+    for index, item in enumerate(file_dict['states']):
+        row = dict()
+        for player_id, player_states in item['game'].items():
+            for attrib, value in player_states.items():
+                row["p_{}_{}".format(player_id, attrib)] = value
+        for p_input, value in item['input'].items():
+            row["input_{}".format(p_input)] = value
+        actual_state_dict[index] = row
+
+    # JSON(actual_state_dict)
+
+    actual_state_df = pd.DataFrame.from_dict(actual_state_dict, orient='index')
+
+    ##display(actual_state_df.head())
+    ##display(actual_state_df.tail())
+
+    return actual_state_df
+
+
+def calculate_reformed_input_df(eval_df, norm_state_df):
+    input_windows = list()
+    for idx, row in eval_df.iterrows():
+        input_window = list()
+        for idx in range(int(row['window_0']), int(row['window_1']) + 1):
+            input_window = input_window + norm_state_df.iloc[idx].to_list()
+        input_windows.append(input_window)
+
+    reformed_input_df = pd.DataFrame(input_windows)
+    reformed_input_df.index = list(reformed_input_df.index)
+    reformed_input_df.columns = list(reformed_input_df.columns)
+    reformed_input_df.columns = reformed_input_df.columns.map(str)
+
+    ##display(reformed_input_df.head())
+    ##display(reformed_input_df.tail())
+
+    return reformed_input_df
+
+
+def create_merged_eval_actual_state_df(eval_df, actual_state_df):
+    merged_eval_actual_state_df = eval_df.merge(
+        actual_state_df,
+        how='left',
+        left_on='window_0',
+        right_index=True
+    )
+
+    ##display(merged_eval_actual_state_df.head())
+    # display(merged_eval_actual_state_df[merged_eval_actual_state_df.isna().any(axis=1)])
+
+    return merged_eval_actual_state_df
+
+
+def generate_diff(merged_eval_actual_state_df):
+    # merged_eval_actual_state_df['p_0_x_posi_diff'] = file_dict['state_format']['minmax']['x_posi']['max'] - merged_eval_actual_state_df['p_0_x_posi']
+    merged_eval_actual_state_df['p_0_x_posi_diff'] = merged_eval_actual_state_df['p_0_x_posi'].diff()
+    merged_eval_actual_state_df['p_0_x_posi_diff'] = merged_eval_actual_state_df['p_0_x_posi_diff'].shift(-1)
+    # display(merged_eval_actual_state_df[merged_eval_actual_state_df.isna().any(axis=1)])
+
+    return merged_eval_actual_state_df
+
+
+def plot_diff(merged_eval_actual_state_df):
+    fig = go.Figure()
+
+    # Add traces
+    fig.add_trace(go.Scatter(
+        x=merged_eval_actual_state_df.index,
+        y=merged_eval_actual_state_df['p_0_x_posi'],
+        mode='lines',
+        name='p_0_x_posi'))
+
+    # fig.show()
+
+    fig = go.Figure()
+
+    # Add traces
+    fig.add_trace(go.Bar(
+        x=merged_eval_actual_state_df.index,
+        y=merged_eval_actual_state_df['p_0_x_posi_diff'],
+        # mode='lines',
+        name='p_0_x_posi_diff'))
+
+    # fig.show()
+
+
+def apply_reward(merged_eval_actual_state_df, reward_column, reward_falloff):
+    reward_values = {}
+    merged_eval_actual_state_df["{}_reward".format(reward_column)] = 0
+    merged_eval_actual_state_df[reward_column].fillna(0,inplace=True)
+
+    for idx, row in merged_eval_actual_state_df[::-1].iterrows():
+        reward_values[idx] = {
+            "fall_off": (row[reward_column]/reward_falloff),
+            "interval": 0,
+            "value": row[reward_column]
+        }
+        actual_reward = 0
+        remove_idx = []
+        for r_key, r_item in reward_values.items():
+            actual_reward = actual_reward + r_item['value'] - (r_item['fall_off'] * r_item['interval'])
+
+            if r_item['interval'] == reward_falloff:
+                remove_idx.append(r_key)
+            else:
+                r_item['interval'] = r_item['interval'] + 1
+        merged_eval_actual_state_df.at[idx, "{}_reward".format(reward_column)] = actual_reward
+
+        for rem_idx in remove_idx:
+            del reward_values[rem_idx]
+
+    return merged_eval_actual_state_df
+
+
+def calculate_reward_df(merged_eval_actual_state_df):
+    reward_columns = ['p_0_x_posi_diff']
+    falloff = 5
+
+    for col in reward_columns:
+        merged_eval_actual_state_df = apply_reward(merged_eval_actual_state_df, reward_column=col, reward_falloff=falloff)
+
+    fig = go.Figure()
+
+    # Add traces
+    fig.add_trace(go.Bar(
+        x=merged_eval_actual_state_df.index,
+        y=merged_eval_actual_state_df['p_0_x_posi_diff'],
+        # mode='lines',
+        name='p_0_x_posi_diff'))
+
+    # Add traces
+    fig.add_trace(go.Scatter(
+        x=merged_eval_actual_state_df.index,
+        y=merged_eval_actual_state_df['p_0_x_posi_diff_reward'],
+        mode='lines',
+        name='p_0_x_posi_diff_reward'))
+
+    # fig.show()
+
+    return merged_eval_actual_state_df
+
+
+def merge_input_with_reward(merged_eval_actual_state_df, reformed_input_df, reward_column):
+    merged_eval_actual_state_df.rename(columns={reward_column: "reward"}, inplace=True)
+    reformed_input_df = reformed_input_df.add_prefix('input_')
+    output_columns = [c for c in merged_eval_actual_state_df.columns if c.startswith('output')]
+    merged_eval_actual_state_df = merged_eval_actual_state_df[output_columns + ['reward']]
+
+    output_with_input_and_reward = merged_eval_actual_state_df.merge(
+        reformed_input_df,
+        right_index=True,
+        left_index=True
+    )
+
+    output_with_input_and_reward = output_with_input_and_reward[:-1]
+
+    return output_with_input_and_reward
+
+
+def caclulate_reward_from_eval(file_path):
+    file_dict = load_file(file_path)
+
+    eval_df = create_eval_df(file_dict)
+
+    norm_state_df = create_norm_state_df(file_dict)
+
+    actual_state_df = calcuate_actual_state_df(file_dict)
+
+    reformed_input_df = calculate_reformed_input_df(eval_df, norm_state_df)
+
+    merged_eval_actual_state_df = create_merged_eval_actual_state_df(eval_df, actual_state_df)
+
+    merged_eval_actual_state_df = generate_diff(merged_eval_actual_state_df)
+
+    plot_diff(merged_eval_actual_state_df)
+
+    merged_eval_actual_state_df = calculate_reward_df(merged_eval_actual_state_df)
+
+    output_with_input_and_reward = merge_input_with_reward(merged_eval_actual_state_df, reformed_input_df,
+                                                           'p_0_x_posi_diff_reward')
+
+    #     display(merged_eval_actual_state_df.head())
+    #     display(reformed_input_df.head())
+    #     display(output_with_input_and_reward.head())
+    #     display(output_with_input_and_reward.tail())
+
+    #     display(output_with_input_and_reward.head())
+    #     display(output_with_input_and_reward.tail())
+
+    return output_with_input_and_reward
+
+
+def generate_json_from_in_out_df(output_with_input_and_reward):
+    json_dict = {}
+
+    output_columns = [c for c in output_with_input_and_reward.columns if c.startswith('output_')]
+    input_columns = [c for c in output_with_input_and_reward.columns if c.startswith('input_')]
+    json_dict['output'] = output_with_input_and_reward[output_columns].values.tolist()
+    json_dict['reward'] = output_with_input_and_reward['reward'].values.tolist()
+    json_dict['input'] = output_with_input_and_reward[input_columns].values.tolist()
+
+    return json_dict
+
+
+def main():
+    run_name = "p1_x_posi_test_1"
+    eval_path = "data/eval/{}".format(run_name)
+
+    onlyfiles = [f for f in listdir(eval_path) if isfile(join(eval_path, f))]
+
+    for file in onlyfiles:
+        if file.startswith('eval_'):
+            reward_file = file.replace('eval_', 'reward_')
+            print(file)
+            df = caclulate_reward_from_eval("{}/{}".format(eval_path, file))
+            file_json = generate_json_from_in_out_df(df)
+
+            with open("{}/{}".format(eval_path, reward_file), 'w') as f_writer:
+                f_writer.write(json.dumps(file_json))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/train_model.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+import torch.utils.data as td
+from torch.autograd import Variable
+
+import json
+from IPython.display import JSON
+import numpy as np
+
+from os import listdir
+from os.path import isfile, join
+
+from tqdm import tqdm
+
+import time
+import datetime
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class Model(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(Model, self).__init__()
+        # self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, output_size),
+        )
+        self.activation = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        # x = self.flatten(x)
+        out = self.linear_relu_stack(x)
+        return self.activation(out)
+
+
+def setup_model(input_layer_size, output_layer_size, learning_rate):
+    model = Model(input_layer_size, output_layer_size)
+    model.share_memory()
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+    return model, optimizer
+
+
+def load_model(model, optimizer):
+    path = "data/eval"
+    model.load_state_dict(
+        torch.load("{}/{}/model/{}.model".format(path, run_name, load_model_file),
+                   map_location=lambda storage, loc: storage))
+    optimizer.load_state_dict(torch.load("{}/{}/model/{}.optim".format(path, run_name, load_model_file)))
+
+
+def load_reward_data():
+    onlyfiles = [f for f in listdir(eval_path) if isfile(join(eval_path, f))]
+
+    full_data = {
+        'input': [],
+        'output': [],
+        'reward': []
+    }
+
+    for file in tqdm(onlyfiles):
+        if file.startswith('reward_'):
+            with open("{}/{}".format(eval_path, file)) as f:
+                file_dict = json.load(f)
+            for k_, i_ in file_dict.items():
+                full_data[k_] = full_data[k_] + i_
+    return full_data
+
+
+def save_model(model, optim):
+    path = "data/eval/{}".format(run_name)
+
+    torch.save(model.state_dict(), "{}/model/{}.model".format(path, dt_str))
+    torch.save(optim.state_dict(), "{}/model/{}.optim".format(path, dt_str))
+
+    print("{}.model saved".format(dt_str))
+    print("{}.optim saved".format(dt_str))
+
+
+def create_dataset(data):
+    input_tensor = torch.Tensor(data['input']).to(device)
+    output_tensor = torch.Tensor(data['output']).to(device)
+    reward_tensor = torch.Tensor(data['reward']).to(device)
+
+    return td.TensorDataset(input_tensor, output_tensor, reward_tensor)
+    # return td.TensorDataset(torch.cat(input_tensor), torch.stack(torch.cat(output_tensor), torch.cat(reward_tensor), dim=1))
+
+
+def train(model, optim, criterion, data):
+    observation = Variable(data[0]).to(device)
+    action_out = model(observation).to(device)
+
+    # actions = Variable(t[:, 0].type(torch.LongTensor))
+    # rewards = Variable(t[:, 1].type(torch.LongTensor))
+    actions = Variable(data[1]).to(device)
+    rewards = Variable(data[2]).to(device)
+
+    # Calculates the loss for the movement outputs and the attack outputs
+    loss = torch.sum(rewards * (criterion(actions, action_out)))
+
+    optim.zero_grad()  # Resets the models gradients
+    loss.backward()
+    optim.step()  # Updates the network weights based on the calculated gradients
+
+
+run_name = 'p1_x_posi_test_1'
+eval_path = "data/eval/{}".format(run_name)
+learning_rate = 1e-5
+epochs = 100
+load_model_file = '20221022_003718'
+dt_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+print("device={}".format(device))
+
+print(torch.cuda.current_device())
+print(torch.cuda.device(0))
+print(torch.cuda.get_device_name(0))
+
+
+reward_data = load_reward_data()
+
+input_size = len(reward_data['input'][0])
+output_size = len(reward_data['output'][0])
+
+model, optim = setup_model(input_size, output_size, learning_rate)
+criterion = nn.CrossEntropyLoss(reduction='none')
+model.to(device)
+
+print(model)
+print(optim)
+
+dataset = create_dataset(reward_data)
+
+# N = len(dataset)
+# data_sampler = td.sampler.RandomSampler(range(N))
+# dataloader = td.DataLoader(dataset, sampler=data_sampler, batch_size=batch_size)
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
+
+# with torch.profiler.profile(
+#         schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+#         on_trace_ready=torch.profiler.tensorboard_trace_handler('../../logs/{}_{}'.format(dt_str, run_name)),
+#         record_shapes=True,
+#         profile_memory=True,
+#         with_stack=True,
+#         activities=[
+#             torch.profiler.ProfilerActivity.CPU,
+#             torch.profiler.ProfilerActivity.CUDA,
+#         ]
+# ) as prof:
+for epoch in tqdm(range(0, epochs)):
+    for step, batch_data in enumerate(train_loader):
+        train(model, optim, criterion, batch_data)
+            # train(batch_data)
+            #prof.step()  # Need to call this at the end of each step to notify profiler of steps' boundary.
+
+print("{} done".format(dt_str))
+
+save_model(model, optim)
