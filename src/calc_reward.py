24ddd24ced98ec2421d01e748d14e5d9f2ee29aa
rkaganda <rkaganda@gmail.com>
2022-10-24 13:59:37 -0700

    updated calc reward

--- src/calc_reward.py
+++ src/calc_reward.py
@@ -1,23 +1,13 @@
 import pandas as pd
 import json
-from IPython.display import JSON
 from os import listdir
 from os.path import isfile, join
-
-import plotly.express as px
-import plotly.graph_objs as go
-from plotly.graph_objs.scatter.marker import Line
-
-pd.options.display.max_rows = 4000
-pd.options.display.max_seq_items = 4000
-pd.options.display.max_columns = 4000
+from pathlib import Path
 
 
 def load_file(file_path):
     with open(file_path) as f:
         file_dict = json.load(f)
-
-    JSON(file_dict)
 
     return file_dict
 
@@ -36,12 +26,7 @@ def create_eval_df(file_dict):
                 row["{}".format(key)] = item
         d_[frame_idx] = row
 
-    # JSON(d_)
-
     eval_df = pd.DataFrame.from_dict(d_, orient='index')
-
-    # display(eval_df.head())
-    # display(eval_df.tail())
 
     return eval_df
 
@@ -55,12 +40,7 @@ def create_norm_state_df(file_dict):
             row[idx] = value
         norm_dict[index] = row
 
-    # JSON(norm_dict)
-
     norm_state_df = pd.DataFrame.from_dict(norm_dict, orient='index')
-
-    # display(norm_state_df.head())
-    # display(norm_state_df.tail())
 
     return norm_state_df
 
@@ -77,19 +57,14 @@ def calcuate_actual_state_df(file_dict):
             row["input_{}".format(p_input)] = value
         actual_state_dict[index] = row
 
-    # JSON(actual_state_dict)
-
     actual_state_df = pd.DataFrame.from_dict(actual_state_dict, orient='index')
-
-    ##display(actual_state_df.head())
-    ##display(actual_state_df.tail())
 
     return actual_state_df
 
 
 def calculate_reformed_input_df(eval_df, norm_state_df):
     input_windows = list()
-    for idx, row in eval_df.iterrows():
+    for jdx, row in eval_df.iterrows():
         input_window = list()
         for idx in range(int(row['window_0']), int(row['window_1']) + 1):
             input_window = input_window + norm_state_df.iloc[idx].to_list()
@@ -99,9 +74,6 @@ def calculate_reformed_input_df(eval_df, norm_state_df):
     reformed_input_df.index = list(reformed_input_df.index)
     reformed_input_df.columns = list(reformed_input_df.columns)
     reformed_input_df.columns = reformed_input_df.columns.map(str)
-
-    ##display(reformed_input_df.head())
-    ##display(reformed_input_df.tail())
 
     return reformed_input_df
 
@@ -114,53 +86,25 @@ def create_merged_eval_actual_state_df(eval_df, actual_state_df):
         right_index=True
     )
 
-    ##display(merged_eval_actual_state_df.head())
-    # display(merged_eval_actual_state_df[merged_eval_actual_state_df.isna().any(axis=1)])
-
     return merged_eval_actual_state_df
 
 
-def generate_diff(merged_eval_actual_state_df):
-    # merged_eval_actual_state_df['p_0_x_posi_diff'] = file_dict['state_format']['minmax']['x_posi']['max'] - merged_eval_actual_state_df['p_0_x_posi']
-    merged_eval_actual_state_df['p_0_x_posi_diff'] = merged_eval_actual_state_df['p_0_x_posi'].diff()
-    merged_eval_actual_state_df['p_0_x_posi_diff'] = merged_eval_actual_state_df['p_0_x_posi_diff'].shift(-1)
-    # display(merged_eval_actual_state_df[merged_eval_actual_state_df.isna().any(axis=1)])
+def generate_diff(merged_eval_actual_state_df, reward_columns):
+    for c, modifer in reward_columns.items():
+        merged_eval_actual_state_df['{}_diff'.format(c)] = merged_eval_actual_state_df[c].diff()
+        merged_eval_actual_state_df['{}_diff'.format(c)] = merged_eval_actual_state_df['{}_diff'.format(c)] * modifer
 
     return merged_eval_actual_state_df
-
-
-def plot_diff(merged_eval_actual_state_df):
-    fig = go.Figure()
-
-    # Add traces
-    fig.add_trace(go.Scatter(
-        x=merged_eval_actual_state_df.index,
-        y=merged_eval_actual_state_df['p_0_x_posi'],
-        mode='lines',
-        name='p_0_x_posi'))
-
-    # fig.show()
-
-    fig = go.Figure()
-
-    # Add traces
-    fig.add_trace(go.Bar(
-        x=merged_eval_actual_state_df.index,
-        y=merged_eval_actual_state_df['p_0_x_posi_diff'],
-        # mode='lines',
-        name='p_0_x_posi_diff'))
-
-    # fig.show()
 
 
 def apply_reward(merged_eval_actual_state_df, reward_column, reward_falloff):
     reward_values = {}
     merged_eval_actual_state_df["{}_reward".format(reward_column)] = 0
-    merged_eval_actual_state_df[reward_column].fillna(0,inplace=True)
+    merged_eval_actual_state_df[reward_column].fillna(0, inplace=True)
 
     for idx, row in merged_eval_actual_state_df[::-1].iterrows():
         reward_values[idx] = {
-            "fall_off": (row[reward_column]/reward_falloff),
+            "fall_off": (row[reward_column] / reward_falloff),
             "interval": 0,
             "value": row[reward_column]
         }
@@ -178,36 +122,24 @@ def apply_reward(merged_eval_actual_state_df, reward_column, reward_falloff):
         for rem_idx in remove_idx:
             del reward_values[rem_idx]
 
-    return merged_eval_actual_state_df
 
+def calculate_reward_df(merged_eval_actual_state_df, reward_columns, falloff):
+    reward_c = []
 
-def calculate_reward_df(merged_eval_actual_state_df):
-    reward_columns = ['p_0_x_posi_diff']
-    falloff = 5
+    for idx, col in enumerate(reward_columns):
+        rc = "{}_diff".format(col)
+        apply_reward(merged_eval_actual_state_df, reward_column=rc, reward_falloff=falloff)
+        reward_c.append("{}_reward".format(rc))
 
-    for col in reward_columns:
-        merged_eval_actual_state_df = apply_reward(merged_eval_actual_state_df, reward_column=col, reward_falloff=falloff)
+    merged_eval_actual_state_df['reward_total'] = 0
 
-    fig = go.Figure()
+    for r in reward_c:
+        merged_eval_actual_state_df['reward_total'] = merged_eval_actual_state_df['reward_total'] + \
+                                                      merged_eval_actual_state_df[r]
 
-    # Add traces
-    fig.add_trace(go.Bar(
-        x=merged_eval_actual_state_df.index,
-        y=merged_eval_actual_state_df['p_0_x_posi_diff'],
-        # mode='lines',
-        name='p_0_x_posi_diff'))
-
-    # Add traces
-    fig.add_trace(go.Scatter(
-        x=merged_eval_actual_state_df.index,
-        y=merged_eval_actual_state_df['p_0_x_posi_diff_reward'],
-        mode='lines',
-        name='p_0_x_posi_diff_reward'))
-
-    # fig.show()
-    merged_eval_actual_state_df['p_0_x_posi_diff_reward'] = \
-        (merged_eval_actual_state_df['p_0_x_posi_diff_reward'] - merged_eval_actual_state_df[
-            'p_0_x_posi_diff_reward'].mean()) / merged_eval_actual_state_df['p_0_x_posi_diff_reward'].std()
+    merged_eval_actual_state_df['reward_total_norm'] = \
+        (merged_eval_actual_state_df['reward_total'] - merged_eval_actual_state_df['reward_total'].mean()) / \
+        merged_eval_actual_state_df['reward_total'].std()
 
     return merged_eval_actual_state_df
 
@@ -229,7 +161,9 @@ def merge_input_with_reward(merged_eval_actual_state_df, reformed_input_df, rewa
     return output_with_input_and_reward
 
 
-def caclulate_reward_from_eval(file_path):
+def caclulate_reward_from_eval(file_path, reward_columns, falloff):
+    print("reward_columns={}".format(reward_columns))
+    print("falloff={}".format(falloff))
     file_dict = load_file(file_path)
 
     eval_df = create_eval_df(file_dict)
@@ -242,22 +176,12 @@ def caclulate_reward_from_eval(file_path):
 
     merged_eval_actual_state_df = create_merged_eval_actual_state_df(eval_df, actual_state_df)
 
-    merged_eval_actual_state_df = generate_diff(merged_eval_actual_state_df)
+    merged_eval_actual_state_df = generate_diff(merged_eval_actual_state_df, reward_columns)
 
-    plot_diff(merged_eval_actual_state_df)
-
-    merged_eval_actual_state_df = calculate_reward_df(merged_eval_actual_state_df)
+    merged_eval_actual_state_df = calculate_reward_df(merged_eval_actual_state_df, list(reward_columns.keys()), falloff)
 
     output_with_input_and_reward = merge_input_with_reward(merged_eval_actual_state_df, reformed_input_df,
-                                                           'p_0_x_posi_diff_reward')
-
-    #     display(merged_eval_actual_state_df.head())
-    #     display(reformed_input_df.head())
-    #     display(output_with_input_and_reward.head())
-    #     display(output_with_input_and_reward.tail())
-
-    #     display(output_with_input_and_reward.head())
-    #     display(output_with_input_and_reward.tail())
+                                                           'reward_total_norm')
 
     return output_with_input_and_reward
 
@@ -275,21 +199,33 @@ def generate_json_from_in_out_df(output_with_input_and_reward):
 
 
 def main():
-    run_name = "p1_x_posi_test"
-    eval_path = "data/eval/{}".format(run_name)
+    run = "p1_vs_cpu_heath"
 
+    eval_path = "data/eval/{}/evals".format(run)
+    reward_path = "data/eval/{}/reward".format(run)
+
+    reward_columns = {
+        "p_0_health": -1,
+        "p_1_health": 1
+    }
+    falloff = 15
+
+    Path("{}".format(reward_path)).mkdir(parents=True, exist_ok=True)
     onlyfiles = [f for f in listdir(eval_path) if isfile(join(eval_path, f))]
 
     for file in onlyfiles:
         if file.startswith('eval_'):
-            print(file)
             reward_file = file.replace('eval_', 'reward_')
-            df = caclulate_reward_from_eval("{}/{}".format(eval_path, file))
+            file_name = "{}/{}".format(eval_path, file)
+            df = caclulate_reward_from_eval(file_name, reward_columns, falloff)
             file_json = generate_json_from_in_out_df(df)
 
-            with open("{}/{}".format(eval_path, reward_file), 'w') as f_writer:
+            with open("{}/{}".format(reward_path, reward_file), 'w') as f_writer:
                 f_writer.write(json.dumps(file_json))
 
 
 if __name__ == "__main__":
     main()
+
+
+
